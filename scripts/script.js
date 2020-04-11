4797b3b6bdaa7e783706a89f30504edd844eb8c4
saitejachen <saitejachen@gmail.com>
2020-04-11 13:31:25 +0530

    Added DOM

--- scripts/script.js
+++ scripts/script.js
@@ -7,37 +7,67 @@ function playRound(playerSelection, computerSelection){
     computerSelection=computerSelection.toLowerCase();
 
     if (playerSelection == "rock" && computerSelection == "paper") {
-        alert(computerSelection);
-        alert("You lost! Paper covers rock!")
+        c++;
+        console.log("You lost! Paper covers rock!")
     }
     else if (playerSelection == "paper" && computerSelection == "rock") {
-        alert(computerSelection);
-        alert("You won! Paper covers rock!");
+        p++;
+        console.log("You won! Paper covers rock!");
     }
     else if (playerSelection == "rock" && computerSelection == "scissors") {
-        alert(computerSelection);
-        alert("You won! rock breaks scissors!");
+        p++;
+        console.log("You won! rock breaks scissors!");
     }
     else if (playerSelection == "scissors" && computerSelection == "rock") {
-        alert(computerSelection);
-        alert("You lost! rock breaks scissors!");
+        c++;
+        console.log("You lost! rock breaks scissors!");
     }
     else if (playerSelection == "paper" && computerSelection == "scissors") {
-        alert(computerSelection);
-        alert("You lost! scissors cuts rock!");
+        c++;
+        console.log("You lost! scissors cuts rock!");
     }
     else if (playerSelection == "scissors" && computerSelection == "paper") {
-        alert(computerSelection);
-        alert("You won! scissors cuts Paper!");
+        p++;
+        console.log("You won! scissors cuts Paper!");
     }
     else {
-        alert("Draw");
+        console.log("Draw");
     }
 }
 
-function game(){
-    let playerChoice=prompt("Choose object ");
+function match(playerChoice){
+    console.log(playerChoice);
+    //let playerChoice=prompt("Choose object ");
     playRound(playerChoice,compChoice());
 }
+var m=0;p=0,c=0;
+function game(){
+    m++;
+    console.log("p:",p);
+    console.log("c:",c);
+    if (m==5 && p>c){
+        console.clear();
+        console.log("Winner : Player");
+        p=0,c=0,m=0;
+    }
+    else if (m==5 && c>p){
+        console.clear();
+        console.log("Winner : Computer");
+        p=0,c=0,m=0;
+    }
+    else if (m==5){
+        console.clear();
+        console.log("DRAW!!!!!!!!!");
+        p=0,c=0,m=0;
+    }
+}
 
-game();
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+
+rock.addEventListener('click' , ()=>{match("rock");game();});
+paper.addEventListener('click' , ()=>{match("paper");game();});
+scissors.addEventListener('click' , ()=>{match("scissors");game();});
+
+
